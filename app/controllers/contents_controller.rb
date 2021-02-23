6102717f0e00288eaf3a5746686d0a06dacc2051
Alex Silva <alex.paulo100@gmail.com>
2021-02-23 15:59:12 -0300

    Adiciona deleção de Conteúdo

--- app/controllers/contents_controller.rb
+++ app/controllers/contents_controller.rb
@@ -1,13 +1,13 @@
 class ContentsController < ApplicationController
     before_action :authenticate_user!
+    before_action :set_content, only: [:show, :edit, :update, :destroy]
+
     def index
         @contents = current_user.contents
     end
 
 
-    def Show
-        @content = Content.find(params[:id])
-    end
+    def Show; end
 
     def new
         @content = Content.new
@@ -23,13 +23,10 @@ class ContentsController < ApplicationController
         end
     end
 
-    def edit
-        @content = Content.find(params[:id])
-    end
+    def edit; end
 
     def update
-        @content = Content.find(params[:id])
-
+        
         if @content.update(content_params)
             redirect_to contents_path, notice: 'Content successfully updated!'
     
@@ -37,8 +34,19 @@ class ContentsController < ApplicationController
             render :edit
         end
     end
-
+    def destroy
+       
+        @content.destroy
+        
+        redirect_to contents_path, notice: 'Content successfully destroyed!'
+    
+    end
     private
+
+    def set_content
+        @content = Content.find(params[:id])
+
+    end
 
     def content_params
         params.require(:content).permit(:title, :description)
