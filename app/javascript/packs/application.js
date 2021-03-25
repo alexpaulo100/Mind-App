c943a11616d185b1f96559c384e445352621bf7c
Alex Silva <alex.paulo100@gmail.com>
2021-03-25 19:48:31 -0300

    aplicando alteração

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -16,7 +16,7 @@ import $ from 'jquery'
 import 'select2'
 
 $(document).on('turbolinks:load', function() {
-    $("slect2").select2({
+    $(".select2").select2({
         tags: true,
         tokenSeparators: [',', ' ']
     })
